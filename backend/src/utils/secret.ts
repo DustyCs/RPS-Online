e21b022f1c096d412266c62b40b84ab4d38b462c
DustyC <123617229+DustyCs@users.noreply.github.com>
2025-06-26 19:20:01 +0800

    Test fix for render3

--- backend/src/utils/secret.ts
+++ backend/src/utils/secret.ts
@@ -6,7 +6,7 @@ dotenv.config();
 
 export const ENVIRONMENT = _.defaultTo(process.env.NODE_ENV, "development");
 export const IS_PRODUCTION = ENVIRONMENT === "production";
-export const APP_PORT = _.defaultTo(process.env.APP_PORT, 5000);
+export const APP_PORT = _.defaultTo(process.env.APP_PORT, 5100);
 export const LOG_DIR = _.defaultTo(process.env.LOG_DIR, path.resolve(__dirname, "/logs"));
 export const JWT_SECRET = _.defaultTo(process.env.JWT_SECRET, "NoCal Sec R22");
 export const MONGODB = {
