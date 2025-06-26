bfb6865c2262e5fc1166bd6fe03fe57307a92025
DustyC <123617229+DustyCs@users.noreply.github.com>
2025-06-26 19:48:05 +0800

    Zzzz head hurts

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,7 +1,7 @@
 import app from "./app";
 import { APP_PORT } from "./utils/secret"
 
-const port = process.env.PORT || APP_PORT;
+const port = process.env.PORT || 4000;
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
